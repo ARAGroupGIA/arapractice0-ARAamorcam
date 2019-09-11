6d4f21da8ccd810545811ba243a7caca7bad1403
ARAamorcam <55189066+ARAamorcam@users.noreply.github.com>
2019-09-11 14:30:10 +0200

    Version1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,12 @@
 /* Here, you should entry the required libraries */
-
+#include <stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
+	int A, B, result;
+	printf("Write the first number");
+	scanf("%d",&A);
+	printf("Write the second number");
+	scanf("%d",&B);
+	result=A+B;
+	printf("The result of the addition is %d", result);
 }
